6780455bd4489f7750d2b776aa967b354f441b73
Shivam Joshi <shivamjoshi0822@gmail.com>
2021-01-05 19:59:06 +0530

    creating get requests

---
+++ src/main/java/com/example/codeengine/expense/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package com.example.codeengine.expense.controller;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.codeengine.expense.model.Category;
+import com.example.codeengine.expense.repository.CategoryRepository;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+	private CategoryRepository categoryRepository;
+
+	public CategoryController(CategoryRepository categoryRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+	}
+
+	
+	@GetMapping("/categories")
+	Collection<Category> categories() {
+		return categoryRepository.findAll();
+	}
+
+	@GetMapping("/category/{id}")
+	ResponseEntity<?> getCategory(@PathVariable Long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+		return category
+				.map(response -> ResponseEntity.ok().body(response)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+}
+

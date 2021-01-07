6c06f3352102aa6c67f7d5fe35096d5f2df24c9b
Shivam Joshi <shivamjoshi0822@gmail.com>
2021-01-07 21:39:08 +0530

    addiing put and delete mapping

--- src/main/java/com/example/codeengine/expense/model/Category.java
+++ src/main/java/com/example/codeengine/expense/model/Category.java
@@ -6,6 +6,7 @@ import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @NoArgsConstructor
@@ -16,6 +17,7 @@ public class Category {
 	@Id
 	private Long id;
 
+	@NonNull
 	private String name;
 
 }
